58fabf8871f2f5d4529190592ee0097a98b5a3bd
AriadnaNatsuki <elizabethcolastra@hotmail.es>
2026-08-13 02:02:40 +0200

    Responsive mobile menu

--- header.js
+++ header.js
@@ -1,5 +1,14 @@
 fetch("header.html")
     .then(response => response.text())
     .then(data => {
+
         document.getElementById("header").innerHTML = data;
+
+        const menuToggle = document.getElementById("menuToggle");
+        const mainNav = document.getElementById("mainNav");
+
+        menuToggle.addEventListener("click", () => {
+            mainNav.classList.toggle("menu-open");
+        });
+
     });
